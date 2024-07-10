82206422aed218644202c93e9d2509375e056178
mahshid vahedi <m.vahedi2025@gmail.com>
2024-07-11 02:30:18 +0330

    call api

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,11 +20,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/pfm', pfmRoutes);
-
+// app.use('/pfm', httpProxy({
+//   target: 'http://localhost:3001',
+//   changeOrigin: true,
+//   pathRewrite: {
+//     '^/pfm': ''
+//   }
+// }));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
