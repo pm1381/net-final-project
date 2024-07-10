c572dfbe471c4fee93bf6181547aec6235f916f4
pm1381 <parham_minoo@yahoo.com>
2024-07-10 14:29:50 +0330

    feat(pfm): Changing database and adding routes

--- app.js
+++ app.js
@@ -3,26 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mysql = require('mysql')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var pfmRoutes   = require('./routes/pfm')
 
 var app = express();
-
-// MySQL connection configuration
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 5432,
-  user: 'root',
-  database: 'money_management'
-});
-// Connect to MySQL
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to MySQL!');
-});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/pfm', pfmRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -53,4 +40,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = {app, connection};
+module.exports = {app};
