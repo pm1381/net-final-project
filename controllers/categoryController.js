a6f8bf1944fd9e6b07e3d3b6c649c32f5f20632c
mahshid vahedi <m.vahedi2025@gmail.com>
2024-07-10 16:22:27 +0330

    add category apis

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,22 +1,113 @@
-var connection  = require('../db/context');
 const asyncHandler = require("express-async-handler");
+const connection = require('../db/context');
+
+exports.add = asyncHandler(async (req, res, next) => {
+  try {
+    const { title } = req.body;
+
+    // Check if the category title already exists
+    const existingCategory = await new Promise((resolve, reject) => {
+      connection.query('SELECT * FROM category WHERE title = ?', [title], (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results[0]);
+        }
+      });
+    });
+
+    if (existingCategory) {
+      return res.status(400).json({ error: 'Category with this title already exists' });
+    }
+
+    // Insert the new category
+    const query = 'INSERT INTO category (title) VALUES (?)';
+    connection.query(query, [title], (err, result) => {
+      if (err) {
+        return next(err);
+      }
+      res.status(201).json({ id: result.insertId, title });
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
 exports.edit = asyncHandler(async (req, res, next) => {
-    //res.send("NOT IMPLEMENTED: notes list");
-    console.log(connection.query)
+  try {
+    const { id } = req.params;
+    const { title } = req.body;
+
+    // Check if the category exists
+    const existingCategory = await new Promise((resolve, reject) => {
+      connection.query('SELECT * FROM category WHERE id = ?', [id], (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results[0]);
+        }
+      });
+    });
+
+    if (!existingCategory) {
+      return res.status(404).json({ error: 'Category not found' });
+    }
+
+    // Update the category
+    const query = 'UPDATE category SET title = ? WHERE id = ?';
+    connection.query(query, [title, id], (err, result) => {
+      if (err) {
+        return next(err);
+      }
+      res.json({ id, title });
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 exports.delete = asyncHandler(async (req, res, next) => {
-    //res.send("NOT IMPLEMENTED: notes list");
-    console.log(connection.query)
-});
+  try {
+    const { id } = req.params;
 
-exports.add = asyncHandler(async (req, res, next) => {
-    //res.send("NOT IMPLEMENTED: notes list");
-    console.log(connection.query)
+    // Check if the category exists
+    const existingCategory = await new Promise((resolve, reject) => {
+      connection.query('SELECT * FROM category WHERE id = ?', [id], (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results[0]);
+        }
+      });
+    });
+
+    if (!existingCategory) {
+      return res.status(404).json({ error: 'Category not found' });
+    }
+
+    // Delete the category
+    const query = 'DELETE FROM category WHERE id = ?';
+    connection.query(query, [id], (err, result) => {
+      if (err) {
+        return next(err);
+      }
+      res.json({ message: 'Category deleted' });
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 exports.watch = asyncHandler(async (req, res, next) => {
-    //res.send("NOT IMPLEMENTED: notes list");
-    console.log(connection.query)
+  try {
+    const query = 'SELECT * FROM category';
+    connection.query(query, (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      res.json(results);
+    });
+  } catch (err) {
+    next(err);
+  }
 });
